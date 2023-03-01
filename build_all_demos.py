8b1a409ef40e7dbdc4054fb0bcff77a082288845
Wayne Lin <wclin@nuvoton.com>
2023-03-01 12:40:44 +0800

    Enlarge LV_MEM size.

--- build_all_demos.py
+++ build_all_demos.py
@@ -2,11 +2,12 @@ import os
 import sys
 
 pro_abs_path = os.path.abspath('.')
-confs = {'800x480x32_RTP', '800x480x32_CTP', '800x480x16_RTP', '800x480x16_CTP', '480x272x32_CTP', '480x272x16_CTP'}
+confs = ['800x480x32_CTP', '800x480x16_RTP', '800x480x16_CTP', '480x272x32_CTP', '480x272x16_CTP', '800x480x32_RTP']
 
+os.system('cd \"'+ pro_abs_path + '\" && scons -c')
 for conf in confs:
-    os.system('cd \"'+ pro_abs_path + '\" && scons -c')
     os.system('cd \"'+ pro_abs_path + '\" && cp config_' + conf + ' .config')
     os.system('cd \"'+ pro_abs_path + '\" && menuconfig --generate --silent')
     os.system('cd \"'+ pro_abs_path + '\" && scons -j 48')
     os.system('cd \"'+ pro_abs_path + '\" && cp rtthread.bin rtthread_' + conf + '.bin')
+    os.system('cd \"'+ pro_abs_path + '\" && scons -c')
